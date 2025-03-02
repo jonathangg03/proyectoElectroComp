1ea6cc0edd49fa5f47c94ee9e1f207a84fcc72bb
MegaByte <MegaByte@ARES>
2025-03-01 22:33:20 -0600

    Limpieza de impresiones

--- src/main/java/com/mycompany/proyecto/Proyecto.java
+++ src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -17,15 +17,10 @@ public class Proyecto {
         Usuario usuarioB = new Usuario("Keneth Gonzalez", "kengo20", "5678df");
         String usuarioIngresado;
         
-        System.out.println("A");
         while(true) {
             System.out.println("B");
             String nombreDeUsuarioAValidar = JOptionPane.showInputDialog("Ingrese el nombre de usuario");
             String claveAValidar = JOptionPane.showInputDialog("Ingrese una contraseña");
-            System.out.println("nombreDeUsuarioAValidar: " + nombreDeUsuarioAValidar);
-            System.out.println("claveAValidar: " + claveAValidar);
-            System.out.println("nombre: " + usuarioA.getNombreDeUsuario());
-            System.out.println("clave: " + usuarioA.getClave());
             usuarioIngresado = validarUsuario(nombreDeUsuarioAValidar, claveAValidar, usuarioA, usuarioB);
             if (usuarioIngresado != ""){
                 JOptionPane.showMessageDialog(null, "Usuario correcto");
@@ -39,7 +34,6 @@ public class Proyecto {
     }
     
     public static String validarUsuario(String usuarioAValidar, String claveAValidar, Usuario usuarioA, Usuario usuarioB){
-        System.out.println(usuarioAValidar.equals(usuarioA.getNombreDeUsuario()));
         if(usuarioAValidar.equals(usuarioA.getNombreDeUsuario()) && claveAValidar.equals(usuarioA.getClave())) {
             return usuarioA.getClave();
         } else if(usuarioAValidar.equals(usuarioB.getNombreDeUsuario()) && claveAValidar.equals(usuarioB.getClave())) {
